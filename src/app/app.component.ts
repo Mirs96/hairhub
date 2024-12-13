d5252000124275c7f737fc3059efe0e543ab636e
Mario Peccio <mariopeccio@hotmail.it>
2024-12-13 18:22:34 +0100

    ciao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = "hairhub";
   isMenuOpen = false;
 
   toggleMenu(): void {
