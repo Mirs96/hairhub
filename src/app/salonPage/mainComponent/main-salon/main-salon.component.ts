f6486954d9d2b471b77bf8334240a2d96bd534b9
Mario Peccio <mariopeccio@hotmail.it>
2024-12-23 16:21:41 +0100

    corretti errori di import in main salon component

--- src/app/salonPage/mainComponent/main-salon/main-salon.component.ts
+++ src/app/salonPage/mainComponent/main-salon/main-salon.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -7,16 +7,17 @@ import { MatMenuModule } from '@angular/material/menu';
 import { SelectServiceComponent } from '../../components/select-service/select-service.component';
 import { CheckoutTableComponent } from '../../components/checkout-table/checkout-table.component';
 import { TreatmentsPriceDetails } from '../../../model/hometables/TreatmentsPricesDetails';
-
+import { Swiper} from 'swiper';
+import { SwiperOptions } from 'swiper/types';
+import { SalonCarouselComponent } from "../../components/salon-carousel/salon-carousel.component";
 
 @Component({
   selector: 'app-main-salon',
-  imports: [RouterLink, RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatIconModule, SelectServiceComponent,CheckoutTableComponent],
+  imports: [RouterLink, RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatIconModule, SalonCarouselComponent, SelectServiceComponent, CheckoutTableComponent],
   templateUrl: './main-salon.component.html',
   styleUrl: './main-salon.component.css'
-  
 })
-export class MainSalonComponent {
+export class MainSalonComponent implements OnInit{
   @Input() selectedTreatments: TreatmentsPriceDetails[] = [];
 
   constructor(private cdRef: ChangeDetectorRef) {}
@@ -25,27 +26,6 @@ export class MainSalonComponent {
     this.selectedTreatments.push(treatment);  // Aggiunge il trattamento alla lista
     this.cdRef.detectChanges(); // Forza la rilevazione dei cambiamenti
   }
-}
-
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { Swiper} from 'swiper';
-import { SwiperOptions } from 'swiper/types';
-import { SalonCarouselComponent } from "../../components/salon-carousel/salon-carousel.component";
-
-
-@Component({
-  selector: 'app-main-salon',
-  imports: [RouterLink, RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatIconModule, SalonCarouselComponent],
-  templateUrl: './main-salon.component.html',
-  styleUrl: './main-salon.component.css'
-})
-export class MainSalonComponent implements OnInit{
-  
   
   
   ngOnInit(): void {
