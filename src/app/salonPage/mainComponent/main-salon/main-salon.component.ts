a2ea201dd5cf46bdd67723cf2983515c75a730f2
Mario Peccio <mariopeccio@hotmail.it>
2024-12-23 15:44:12 +0100

    aggiornamento carosello

--- src/app/salonPage/mainComponent/main-salon/main-salon.component.ts
+++ src/app/salonPage/mainComponent/main-salon/main-salon.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { Swiper} from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 
 @Component({
@@ -12,5 +14,24 @@ import { MatMenuModule } from '@angular/material/menu';
   templateUrl: './main-salon.component.html',
   styleUrl: './main-salon.component.css'
 })
-export class MainSalonComponent {
+export class MainSalonComponent implements OnInit{
+  
+  
+  
+  ngOnInit(): void {
+    const swiperOptions: SwiperOptions = {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    };
+
+    const swiper = new Swiper('.swiper', swiperOptions);
+    }
+
 }
