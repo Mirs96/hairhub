6ae723b2d34a0ec5c2ec7967dc8b624ca7eb7f11
Miryam Massa <miryammassa@gmail.com>
2025-01-11 16:29:23 +0100

    aggiunta la pulizia della checkout-table un volta chiusa la finestra della prenotazione

--- src/app/salonPage/components/checkout-table/checkout-table.component.ts
+++ src/app/salonPage/components/checkout-table/checkout-table.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import {CdkDrag} from '@angular/cdk/drag-drop';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 import { TreatmentsPriceDetails } from '../../../model/hometables/TreatmentsPricesDetails';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { MatListItem } from '@angular/material/list';
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../../../home/login/login.component';
@@ -15,20 +15,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-table',
-  imports: [CdkDrag,MatListModule, MatDividerModule,MatIcon,CommonModule, MatDialogModule],
+  imports: [CdkDrag, MatListModule, MatDividerModule, MatIcon, CommonModule, MatDialogModule],
   templateUrl: './checkout-table.component.html',
   styleUrl: './checkout-table.component.css',
 })
-export class CheckoutTableComponent implements OnChanges{
+export class CheckoutTableComponent implements OnChanges {
   barberFlag: boolean = false;
 
 
-  constructor(private cdRef: ChangeDetectorRef, private dialog: MatDialog, private route: ActivatedRoute) {}
+  constructor(private cdRef: ChangeDetectorRef, private dialog: MatDialog, private route: ActivatedRoute) { }
 
-  @Input() 
-  selectedTreatments!: TreatmentsPriceDetails[] ;
+  @Input()
+  selectedTreatments!: TreatmentsPriceDetails[];
   @Output()
-   treatmentRemoved = new EventEmitter<TreatmentsPriceDetails>();
+  treatmentRemoved = new EventEmitter<TreatmentsPriceDetails>();
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedTreatments']) {
@@ -38,7 +38,7 @@ export class CheckoutTableComponent implements OnChanges{
 
   get total(): number {
     return this.selectedTreatments.reduce((sum, treatment) => sum + treatment.price, 0);
-    
+
   }
   removeTreatment(id: number): void {
     const index = this.selectedTreatments.findIndex(treatment => treatment.id === id);
@@ -49,19 +49,20 @@ export class CheckoutTableComponent implements OnChanges{
     }
   }
 
-  OnClickBarber(){
+  OnClickBarber() {
     const dialogRef = this.dialog.open(BookingComponent, {
-            width: '0px',
-            height: '0px',
-            data: { id: this.route.snapshot.paramMap.get("id"),selectedTreatments: this.selectedTreatments }
-        });
+      width: '0px',
+      height: '0px',
+      data: { id: this.route.snapshot.paramMap.get("id"), selectedTreatments: this.selectedTreatments }
+    });
 
-        dialogRef.afterClosed().subscribe(result => {
-            console.log('Dialogo chiuso', result);
+    dialogRef.afterClosed().subscribe(result => {
+      this.selectedTreatments = [];
+      console.log('Dialogo chiuso', result);
 
-            if (result && result.success) {
-              console.log('Prenotazione effettuata');
-            }
-        });
+      if (result && result.success) {
+        console.log('Prenotazione effettuata');
+      }
+    });
   }
 }
